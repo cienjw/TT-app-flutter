58ae3a10d247ea4ccf2f8fee49a8b134e0136b31
jiwon <cienjw@gmail.com>
2026-06-15 17:12:46 +0900

    fix: ios client id 수정

---
+++ lib/core/constants/ble_constants.dart
@@ -0,0 +1,5 @@
+class BleConstants {
+  // 우리 앱 전용 식별자 (아무 폰이나 잡지 않게 고정)
+  static const serviceUuid = 'f47ac10b-58cc-4372-a567-0e02b2c3d479';
+  static const manufacturerId = 0xFFFF; // 광고에 user_id 실을 슬롯
+}
